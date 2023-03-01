faf94b6fa92af4ec9273b54ef1b30ad0b7d686c9
Izahhoney <2020489338@student.uitm.edu.my>
2023-03-01 20:33:41 +0800

    config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "online_ordering";
+$dbname= "ordering_system";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
