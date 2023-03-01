d8298acbd326853bd96596dfbd3d5876db213436
Izahhoney <2020489338@student.uitm.edu.my>
2023-03-01 19:41:16 +0800

    registeragent

--- Agent/agentdataregister.php
+++ Agent/agentdataregister.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'config.php';
 
-$sql="INSERT INTO register ( name, age, gender, email)
-values ('$_POST[name]','$_POST[age]','$_POST[gender]','$_POST[email]')";
+$sql="INSERT INTO agent (name, email, password, nohp)
+values ('$_POST[name]','$_POST[email]','$_POST[password]','$_POST[nohp]')";
 	if (!mysqli_query($conn,$sql)){
 		
 		die ('Error: ' .mysqli_error($conn));
@@ -13,7 +13,7 @@ values ('$_POST[name]','$_POST[age]','$_POST[gender]','$_POST[email]')";
 
 
 echo "<script
-type='text/jscript'>alert('Register has been submitted!')</script>";
+type='text/jscript'>alert('Register agent has been submitted!')</script>";
 header('refresh:1 url=index.php');
 
 ?>
