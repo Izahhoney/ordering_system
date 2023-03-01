292e7aef6c41d476e876cb76812bc0e674f5b635
Izahhoney <2020489338@student.uitm.edu.my>
2023-03-01 20:32:47 +0800

    agentdataregister

--- agentdataregister.php
+++ agentdataregister.php
@@ -4,10 +4,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'config.php';
 
-$sql="INSERT INTO Agent (name, email, psw, nohp)
-values ('$_POST[name]','$_POST[email]','$_POST[psw]','$_POST[nohp]')";
-	if (!mysqli_query($conn,$sql)) 
-	{
+$sql="INSERT INTO Agent (name, email, password, nohp)
+values ('$_POST[name]','$_POST[email]','$_POST[password]','$_POST[nohp]')";
+	if (!mysqli_query($conn,$sql)){
 		
 		die ('Error: ' .mysqli_error($conn));
 	}
--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "db";
+$dbname= "online_ordering";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
