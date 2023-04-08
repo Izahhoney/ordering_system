ab47083637c31c1aeef0090eec85940e36d63109
Izahhoney <2020489338@student.uitm.edu.my>
2023-04-08 13:33:16 +0800

    c

--- Manager/registerstaff.php
+++ Manager/registerstaff.php
@@ -1,63 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTER | AGENT</title>
 </head>
 <body>
-<?php 
-
-if(!session_id())
-{
-session_start();
-}
-include('config.php');
-
-?>
-
-<form class="form" action="staffdataregister.php" method="POST">
-<form action="staffdataregister.php">
-
- 
-    <h1>Register Staff</h1>
-    <p>Please fill in this form.</p>
+<form method="post" action="agentdataregister.php">
+  <div class="staff container">
+    <h1>Staff Registration</h1>
+    <p>Please fill in this form to create an account.</p>
     <hr>
 
-   
-    <label for="staffID"><b>StaffID</b></label>
-    <input type="text" placeholder="Enter ID" name="staffID" id="staffID" required><br></br>
+    <label for="id"><b>Staff ID:</b></label>
+    <input type="text" placeholder="Enter Staff Id" name="staffID" id="staff_name" required>
+    <hr>
 
+    <label for="fullname"><b>Name:</b></label>
+    <input type="text" placeholder="Enter name" name="staff_name" id="staff_name" required>
+    <hr>
 
-    <label for="fullname"><b>Fullname</b></label>
-    <input type="text" placeholder="Enter a name" name="fullname" id="fullname" required><br></br>
+    <label for="email"><b>Email:</b></label>
+    <input type="text" placeholder="Enter email" name="staff_email" id="staff_email" required>
+    <hr>
 
-    <label for="phoneno"><b>Phoneno</b></label>
-    <input type="text" placeholder="Enter Phoneno" name="phoneno" id="phoneno" required><br></br>
+    <label for="address"><b>Address:</b></label>
+    <input type="text" placeholder="Enter address" name="staff_address" id="staff_address" required>
+    <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter email" name="email" id="email" required><br></br>
+    <label for="phoneno"><b>HP Number:</b></label>
+    <input type="text" placeholder="Enter Email" name="staff_phoneno" id="staff_phoneno" required>
+    <hr>
 
-    <label for="position"><b>Position:</b></label>
-    <select name="position">
+    <label for="position"><b>Choose a Position:</b></label>
+    <select name="position" id="carsstaff_position">
     <option value="Staff">Staff</option>
     <option value="Manager">Manager</option>
-    </select></p>
+  </select><br><br>
 
-    <label for="Address"><b>Address:</b></label>
-    <input type="text" placeholder="Enter Address" name="Address" id="Address" required><br></br>
-
-
-    <label for="Password"><b>Password:</b></label>
-    <input type="Password" placeholder="Enter Password" name="Password" id="Password" required><br></br>
+    <label for="password"><b>Password:</b></label>
+    <input type="password" placeholder="Enter Password" name="staff_password" id="staff_password" required>
+    <hr>
 
     <label for="Password-repeat"><b>Repeat Password:</b></label>
     <input type="Password" placeholder="Repeat Password" name="password-repeat" id="Password-repeat" required><br></br>
-   
+    <hr>
 
-    <button type="submit" class="registerbtn">Register</button><br></br>
-   
+    <input type="submit" value="Register">
+  </div>
 
-    <p>By creating an account you agree to our <a href="login.php">Terms & Privacy</a>.</p><br></br>
-
-  </form>
+  <div class="container signin">
+    <p>Already have an account? <a href="registeragent.php">Log in</a>.</p>
+  </div>
+</form>
 </body>
 </html>
