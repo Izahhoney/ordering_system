1d7eb04cda49879e62b551ef00e87851be874d44
Izahhoney <2020489338@student.uitm.edu.my>
2023-04-08 13:39:44 +0800

    COMMIT

--- Manager/registerstaff.php
+++ Manager/registerstaff.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>REGISTER | AGENT</title>
+    <title>REGISTER | STAFF</title>
 </head>
 <body>
 <form method="post" action="staffdataregister.php">
