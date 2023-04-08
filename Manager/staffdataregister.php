7387286ab3f342c03ccddce25b439af1fe993361
Izahhoney <2020489338@student.uitm.edu.my>
2023-04-08 12:31:05 +0800

    c

--- Manager/staffdataregister.php
+++ Manager/staffdataregister.php
@@ -11,7 +11,7 @@ $phone = filter_input(INPUT_POST, 'staff_phoneno');
 $password = filter_input(INPUT_POST, 'staff_password');
 $position = filter_input(INPUT_POST, 'staff_position');
 
-$sql="INSERT INTO Staff (staffID,staff_name,staff_email, staff_address, staff_phoneno,staff_password,staff_position)
+$sql="INSERT INTO staff (staffID,staff_name,staff_email, staff_address, staff_phoneno,staff_password,staff_position)
 values ('$_POST[staffID]','$_POST[staff_name]','$_POST[staff_address]','$_POST[staff_phoneno]','$_POST[staff_password]','$_POST[staff_position]')";
 	if (!mysqli_query($conn,$sql)){
 		
