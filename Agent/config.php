012e62a785fe080117180869669da6426a8cb761
Izahhoney <2020489338@student.uitm.edu.my>
2023-04-28 14:16:39 +0800

    c

--- Agent/config.php
+++ Agent/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "Final_Project";
+$dbname= "finalproject";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
--- Staff/config.php
+++ Staff/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname= "Final_Project";
+$dbname= "fnalproject";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
